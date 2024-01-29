332cc14d9ddeebc40ad8ae927294ac6b6546343b
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-01-29 14:51:40 +0100

    feat: wip register

--- my-little-mvc/register.php
+++ my-little-mvc/register.php
@@ -1,1 +1,30 @@
 <?php
+
+require 'vendor/autoload.php';
+
+use App\Model\User;
+
+if (isset($_POST['submit'])) {
+
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Shop</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<h1>Register</h1>
+<form action="" method="post">
+    <input type="text" name="fullname" id="fullname" placeholder="fullname">
+    <input type="email" name="email" id="email" placeholder="email">
+    <input type="password" name="password" id="password" placeholder="password">
+    <input type="submit" value="submit" name="submit">
+</form>
+</body>
+</html>
--- my-little-mvc/src/Controller/AuthenticationController.php
+++ my-little-mvc/src/Controller/AuthenticationController.php
@@ -4,5 +4,8 @@ namespace App\Controller;
 
 class AuthenticationController
 {
+    public function register(string $email,string $password,string $fullname)
+    {
 
+    }
 }
