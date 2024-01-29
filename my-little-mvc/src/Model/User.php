48959a7416afdae92fcc03132765896de9c6f524
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-01-29 14:50:12 +0100

    feat: create class user

--- my-little-mvc/src/Model/User.php
+++ my-little-mvc/src/Model/User.php
@@ -4,5 +4,128 @@ namespace App\Model;
 
 class User
 {
+    private ?int $id;
+    private ?string $fullname;
+    private ?string $email;
+    private ?string $password;
+    private ?array $role;
+    private ?\DateTime $created_at;
+    private ?\DateTime $updated_at;
 
+    public function __construct(?int $id = null, ?string $fullname = null, ?string $email = null, ?string $password = null, ?array $role = null, ?\DateTime $created_at = null, ?\DateTime $updated_at = null)
+    {
+        $this->id = $id;
+        $this->fullname = $fullname;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+        $this->created_at = $created_at;
+        $this->updated_at = $updated_at;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getFullname(): ?string
+    {
+        return $this->fullname;
+    }
+
+    public function setFullname(?string $fullname): void
+    {
+        $this->fullname = $fullname;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getRole(): ?array
+    {
+        return $this->role;
+    }
+
+    public function setRole(?array $role): void
+    {
+        $this->role = $role;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?\DateTime $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTime $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+
+    public function findOneByEmail(string $email): ?User
+    {
+        $query = $this->pdo->prepare('SELECT * FROM user WHERE email = :email');
+        $query->execute(['email' => $email]);
+        $user = $query->fetchObject(User::class);
+        if ($user === false) {
+            return null;
+        }
+        return $user;
+    }
+
+    public function create(): User
+    {
+        $query = $this->pdo->prepare('INSERT INTO user (fullname, email, password, role, created_at) VALUES (:fullname, :email, :password, :role, :created_at)');
+        $query->execute([
+            'fullname' => $this->fullname,
+            'email' => $this->email,
+            'password' => $this->password,
+            'role' => $this->role,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s')
+        ]);
+        $this->id = $this->pdo->lastInsertId();
+        return $this;
+    }
+
+    public function hydrate(array $data): static
+    {
+        $this->id = $data['id'];
+        $this->fullname = $data['fullname'];
+        $this->email = $data['email'];
+        $this->password = $data['password'];
+        $this->role = json_decode($data['role'], true);
+        $this->created_at = new \DateTime($data['created_at']);
+        $this->updated_at = isset($data['updated_at']) ? new \DateTime($data['updated_at']) : null;
+    }
 }
