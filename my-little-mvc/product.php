93202615bef1742f5b9f170cf382e87ed6dad663
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-01-29 14:50:57 +0100

    feat: register function on product.php

--- my-little-mvc/product.php
+++ my-little-mvc/product.php
@@ -1,1 +1,19 @@
 <?php
+require_once 'vendor/autoload.php';
+use App\Model\Clothing;
+use App\Model\Electronic;
+
+/* recupere les parametre de l'url */
+
+$url_idProduct = $GET['id_product'] ?? null;
+$url_product_type = $GET['product_type'] ?? null;
+
+if ($url_idProduct !== null && intval($url_idProduct) && $url_product_type !== null) {
+    if ($url_product_type === 'clothing') {
+        $product = new Clothing();
+    } elseif ($url_product_type === 'electronic') {
+        $product = new Electronic();
+    } else {
+        throw new Exception('Product type not found');
+    }
+}
