a72d574a4d6d1d0386d4d8dcbcaa5b985706277c
rabaou-ibrahim <rabaou.ibrahim@laplateforme.io>
2024-01-30 16:53:31 +0100

    job03

--- my-little-mvc/product.php
+++ my-little-mvc/product.php
@@ -1,1 +1,28 @@
 <?php
+
+require 'vendor/autoload.php';
+
+use App\Model\Abstract\AbstractProduct;
+use App\Model\Clothing;
+use App\Model\Electronic;
+use App\Model\Category;
+
+$electronic = new Electronic();
+$clothing = new Clothing();
+
+if (isset($_GET['id_product'])){
+
+    $id_product = intval($_GET['id_product']);
+    $electronic = $electronic->findOneById($id_product);
+    $clothing = $clothing->findOneById($id_product);
+
+    if ($electronic){
+        var_dump($electronic);
+    }
+    else if ($clothing){
+        var_dump($clothing);
+    }
+    else {
+        echo "Produit introuvable";
+    }
+}
