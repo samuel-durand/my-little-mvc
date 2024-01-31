51fe4a0583d8f2a551b244534144d7d8cb438641
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-01-31 15:45:37 +0100

    fix: header

--- my-little-mvc/import/header.php
+++ my-little-mvc/import/header.php
@@ -1,21 +1,23 @@
-
 <?php
 
 use App\Controller\AuthenticationController;
 $authController = new AuthenticationController();
+
+$user = $_SESSION['user'] ?? null;
+
 ?>
 
 
 <header>
     <nav class="flex py-2 px-4 border-b-2">
         <ul class="flex justify-between w-full">
-            <li><a href="/shop.php">Accueil</a></li>
+            <li><a href="/my-little-mvc/my-little-mvc/shop.php">Accueil</a></li>
             <?php if ($authController->isLogged()) : ?>
-                <li><a href="/profile.php">Profil</a></li>
-                <li><a href="/logout.php">Déconnexion</a></li>
+                <li><a href="/my-little-mvc/my-little-mvc/profile.php"><?= $user->getFullname();?></a></li>
+                <li><a href="/my-little-mvc/my-little-mvc/logout.php">Déconnexion</a></li>
             <?php else: ?>
-                <li><a href="/login.php">Connexion</a></li>
-                <li><a href="/register.php"></a></li>
+                <li><a href="/my-little-mvc/my-little-mvc/login.php">Connexion</a></li>
+                <li><a href="/my-little-mvc/my-little-mvc/register.php">Inscription</a></li>
             <?php endif; ?>
         </ul>
     </nav>
