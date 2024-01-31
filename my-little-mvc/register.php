bb31abb3f1ea255a1946533d43c49dc03041d35e
rabaou-ibrahim <rabaou.ibrahim@laplateforme.io>
2024-01-31 11:18:28 +0100

    job 05

---
+++ my-little-mvc/register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <form method="post">
+        <label for="fullname">Nom et Prénom</label>
+        <input type="text" name="fullname" id="fullname">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password">
+        <label for="password_confirm">Confirmer le mot de passe</label>
+        <input type="password" name="password_confirm" id="password_confirm">
+        <input type="submit" value="S'inscrire">
+    </form>
+</body>
+</html>
