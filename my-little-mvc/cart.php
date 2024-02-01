553ea1773582d0af9ed263d0768c442552b1b9d9
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-02-01 23:24:30 +0100

    feat: wip cart pgae

---
+++ my-little-mvc/cart.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'vendor/autoload.php';
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Shop - Cart</title>
+</head>
+<body>
+  <?php require_once 'import/header.php'; ?>
+  <main>
+    <h1>Cart</h1>
+  </main>  
+</body>
+</html>
