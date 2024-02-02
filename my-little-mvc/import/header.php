7c5123297938fb83f46b94131bbc708e9bd90d64
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-02-02 12:21:58 +0100

    fix: cart link header

--- my-little-mvc/import/header.php
+++ my-little-mvc/import/header.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Controller\AuthenticationController;
+
 $authController = new AuthenticationController();
 
 $user = $_SESSION['user'] ?? null;
 
+$article = 0;
 ?>
 
 
@@ -24,7 +26,25 @@ $user = $_SESSION['user'] ?? null;
                     <?php if ($authController->isLogged()) : ?>
                         <li class="rounded bg-[#ff2850] border-2 border-[#ff2850] p-2">
                             <a href="/my-little-mvc/my-little-mvc/profile.php">
-                                <?= $user->getFullname();?>
+                                <?= $user->getFullname(); ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/my-little-mvc/my-little-mvc/cart.php" class="flex items-center gap-x-0.5 rounded bg-[#ff2850] border-2 border-[#ff2850] p-2">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         class="icon icon-tabler icon-tabler-shopping-cart" width="24" height="24"
+                                         viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                         stroke-linecap="round" stroke-linejoin="round">
+                                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                      <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
+                                      <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/>
+                                      <path d="M17 17h-11v-14h-2"/>
+                                      <path d="M6 5l14 1l-1 7h-13"/>
+                                    </svg>
+                                </span>
+                                <span>Panier</span>
+                                <span><?php echo '(' . $article . ')'; ?></span>
                             </a>
                         </li>
                         <li class="rounded bg-[#ff2850] border-2 border-[#ff2850] p-2">
