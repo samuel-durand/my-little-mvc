e4941d3033847f66d2ab4d4294c0ab9507dc1bb6
rabaou-ibrahim <rabaou.ibrahim@laplateforme.io>
2024-02-19 16:31:20 +0100

    pagination fin job 10

---
+++ my-little-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nico-rvl/revisions-poo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4fe697afe8fa5ab8169d7145d0126fbac358e166',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nico-rvl/revisions-poo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4fe697afe8fa5ab8169d7145d0126fbac358e166',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
